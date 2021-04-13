fb48249f0932d9f3bcd6260994e8d89ad35d75bd
Júlia Pôrto <juportob@gmail.com>
2021-04-13 13:54:43 -0300

    feat: add database

--- models/Connection.js
+++ models/Connection.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-console.log(process.env.HOSTNAME, 'host')
-
 const config = {
   host: 'us-cdbr-east-03.cleardb.com',
   user: 'bd4924fa610466',
   password: 'edf0e894',
-  database: 'heroku_d5a595fd89af',
+  database: 'heroku_d5a595fd89af2b4',
 };
+
+mysql://bd4924fa610466:edf0e894@us-cdbr-east-03.cleardb.com/heroku_d5a595fd89af2b4?reconnect=true
 
 const connection = mysql.createPool(config);
 
