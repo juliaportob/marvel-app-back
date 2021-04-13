439ebee3b749e1fa276f9d27cee4cd90afc5e0b7
Júlia Pôrto <juportob@gmail.com>
2021-04-13 14:14:50 -0300

    feat: add database 2

--- models/Connection.js
+++ models/Connection.js
@@ -8,8 +8,6 @@ const config = {
   database: 'heroku_d5a595fd89af2b4',
 };
 
-//mysql://bd4924fa610466:edf0e894@us-cdbr-east-03.cleardb.com/heroku_d5a595fd89af2b4?reconnect=true
-
 const connection = mysql.createPool(config);
 
 module.exports = connection;
